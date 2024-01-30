af1a0073dfe1bbd026d036b1c29d7e28ee650eb2
bashtag <btstsiy@hotmail.com>
2024-01-30 16:07:52 +0300

    Scanner/Index route config

--- BarcodeReaderMVC/App_Start/RouteConfig.cs
+++ BarcodeReaderMVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BarcodeReaderMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Scanner", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
